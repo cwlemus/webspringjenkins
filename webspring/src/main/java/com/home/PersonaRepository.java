8239b5909b496df6cfba824617a0fd37c1b1d147
Carlos Lemus <cwlemus@gmail.com>
2024-08-29 18:43:27 -0600

    nuevos

---
+++ webspring/src/main/java/com/home/PersonaRepository.java
@@ -0,0 +1,24 @@
+package com.home;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonaRepository {
+	private List personas = new ArrayList<Persona>();
+	
+	public PersonaRepository() {
+		personas.add(new Persona("ana"));
+	}
+	
+	public void add(Persona persona) {
+		personas.add(persona);
+	}
+	
+	public List<Persona> buscarTodo(){
+		return personas;
+	}
+	
+}
